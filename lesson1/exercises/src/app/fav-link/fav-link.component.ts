a71aee7d4daa51082517e0cb64ddb4dbfcdc2878
romeo adjam <sodangbe@users.noreply.github.com>
2020-12-21 02:56:31 -0600

    finish exercise

--- lesson1/exercises/src/app/fav-link/fav-link.component.ts
+++ lesson1/exercises/src/app/fav-link/fav-link.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-fav-link',
+  selector: 'fav-link',
   templateUrl: './fav-link.component.html',
   styleUrls: ['./fav-link.component.css']
 })
 export class FavLinkComponent implements OnInit {
-
+  favLinks = ['https://techcrunch.com/','https://www.wired.com/'];
   constructor() { }
 
   ngOnInit() {
